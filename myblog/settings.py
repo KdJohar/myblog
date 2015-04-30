1777396b3c626cd0d47e5acada554f98af85baae
kdjohar <karandeepsingh9807@gmail.com>
2015-04-30 11:31:41 +0530

    gitignore added

--- myblog/settings.py
+++ myblog/settings.py
@@ -99,7 +99,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.request",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+)
 
 CKEDITOR_UPLOAD_PATH = "editor_uploads/"
 CKEDITOR_IMAGE_BACKEND = 'pillow'
